9b85781615ddbb17719d732e6e931afd2e5a2ac0
Pedro Simões <pedrosimoes@ipvc.pt>
2024-11-08 16:36:03 +0000

    1.4

--- app/src/main/java/com/example/ipvcconnect/Cursos/Enfermagem.kt
+++ app/src/main/java/com/example/ipvcconnect/Cursos/Enfermagem.kt
@@ -1,6 +1,7 @@
 package com.example.ipvcconnect.Cursos
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -18,4 +19,8 @@ class Enfermagem : AppCompatActivity() {
             insets
         }
     }
+
+    fun UnilabsClick(view: View) {}
+    fun IPOClick(view: View) {}
+    fun BragaClick(view: View) {}
 }
