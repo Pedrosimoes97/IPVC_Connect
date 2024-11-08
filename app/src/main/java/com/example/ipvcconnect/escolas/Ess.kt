3260cc2c67b74860b773b522c606a303399ef563
TurboSlayer98 <hffm98@hotmail.com>
2024-11-08 15:56:37 +0000

    #4

---
+++ app/src/main/java/com/example/ipvcconnect/escolas/Ess.kt
@@ -0,0 +1,24 @@
+package com.example.ipvcconnect.escolas
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.ipvcconnect.R
+
+class Ess : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_ess)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.ess)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+
+    fun enfermagemClick(view: View) {}
+}
