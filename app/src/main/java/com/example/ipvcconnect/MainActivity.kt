a8a3ecbe29c3ed5362baa6f78f3bdf989cf0472f
TurboSlayer98 <hffm98@hotmail.com>
2024-11-08 19:09:11 +0000

    #7

--- app/src/main/java/com/example/ipvcconnect/MainActivity.kt
+++ app/src/main/java/com/example/ipvcconnect/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.ipvcconnect
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import androidx.appcompat.app.AppCompatDelegate
+import com.example.ipvcconnect.escolas.Esa
+import com.example.ipvcconnect.escolas.Esce
+import com.example.ipvcconnect.escolas.Esdl
+import com.example.ipvcconnect.escolas.Ese
+import com.example.ipvcconnect.escolas.Ess
 import com.example.ipvcconnect.escolas.Estg
 
 class MainActivity : AppCompatActivity() {
@@ -26,26 +30,32 @@ class MainActivity : AppCompatActivity() {
 
     fun esdlClick(view: View) {
 
-        Toast.makeText(this, "ESDL", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, Esdl::class.java)
+        startActivity(intent)
     }
     fun esceClick(view: View) {
 
-        Toast.makeText(this, "ESCE", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, Esce::class.java)
+        startActivity(intent)
     }
     fun essClick(view: View) {
 
-        Toast.makeText(this, "ESS", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, Ess::class.java)
+        startActivity(intent)
     }
     fun esaClick(view: View) {
 
-        Toast.makeText(this, "ESA", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, Esa::class.java)
+        startActivity(intent)
     }
     fun eseClick(view: View) {
 
-        Toast.makeText(this, "ESE", Toast.LENGTH_SHORT).show()
+        val intent = Intent(this, Ese::class.java)
+        startActivity(intent)
     }
     fun estgClick(view: View) {
-        val intent = Intent(applicationContext, Estg::class.java)
+
+        val intent = Intent(this, Estg::class.java)
         startActivity(intent)
     }
 
